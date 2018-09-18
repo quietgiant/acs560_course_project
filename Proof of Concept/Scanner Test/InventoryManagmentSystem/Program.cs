fc3b9754850db915d08bd207c177df10750ab28d
Amrit Pal Banwait <palibanwait24@gmail.com>
2018-09-18 19:00:09 -0400

    project

---
+++ Proof of Concept/Scanner Test/InventoryManagmentSystem/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InventoryManagmentSystem
+{
+    class Program
+    {
+        public static void Main()
+        {
+            bool Continue = true;
+            SerialPort mySerialPort = new SerialPort();
+
+            mySerialPort.PortName = "COM3";
+            mySerialPort.BaudRate = 9800;
+
+            mySerialPort.Open();
+
+   
+       
+            while (Continue)
+            {
+                
+                using (StreamWriter writer = new StreamWriter("TestLog.txt", true))
+                {
+
+                    string msg = mySerialPort.ReadLine();
+                    writer.Write("PLU: " + msg);
+
+                }
+            }
+
+        }
+    }
+}
